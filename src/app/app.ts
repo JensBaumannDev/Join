d82765572a4f782693a10d3487c6e403a80a0db7
JensBaumannDev <baumannjens@outlook.de>
2026-04-24 19:51:20 +0200

    feat: initialize project structure with core components, pages, services, and SCSS styling architecture and _reset.scss

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Header } from './components/header/header';
+import { AddTask } from "./components/add-task/add-task";
+import { Summary } from "./components/summary/summary";
+import { Board } from "./components/board/board";
+import { Contacts } from "./components/contacts/contacts";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar, Header, AddTask, Summary, Board, Contacts],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
