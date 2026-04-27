fc726c9a3f5ead6b8fe59a3235113aee516dad21
JensBaumannDev <baumannjens@outlook.de>
2026-04-27 11:04:21 +0200

    feat: scaffold contact-related components and toast notification component

---
+++ src/app/components/contacts/contact-overlay/contact-overlay.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-overlay',
+  imports: [],
+  templateUrl: './contact-overlay.html',
+  styleUrl: './contact-overlay.scss',
+})
+export class ContactOverlay {}
