2f561c089c713c5116d1be44e105a347da7528fd
JensBaumannDev <baumannjens@outlook.de>
2026-04-27 21:03:10 +0200

    feat: scaffold core app structure, routing, and new Help, Login, and ContactOverlay pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Summary } from './pages/summary/summary';
+import { Board } from './pages/board/board';
+import { AddTask } from './pages/add-task/add-task';
+import { Contacts } from './pages/contacts/contacts';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
+import { LegalNotice } from './pages/legal-notice/legal-notice';
+import { Help } from './pages/help/help';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'summary', component: Summary },
+  { path: 'board', component: Board },
+  { path: 'add-task', component: AddTask },
+  { path: 'contacts', component: Contacts },
+  { path: 'privacy-policy', component: PrivacyPolicy },
+  { path: 'legal-notice', component: LegalNotice },
+  { path: 'help', component: Help },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './components/sidebar/sidebar';
 import { Header } from './components/header/header';
-import { AddTask } from "./components/add-task/add-task";
-import { Summary } from "./components/summary/summary";
-import { Board } from "./components/board/board";
-import { Contacts } from "./components/contacts/contacts";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar, Header, AddTask, Summary, Board, Contacts],
+  imports: [RouterOutlet, Sidebar, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
