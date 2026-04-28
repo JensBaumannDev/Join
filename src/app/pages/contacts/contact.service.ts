cc85860dcd05db338f97b1ca189c43bf6564ae3b
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-28 12:19:22 +0200

    feat: implement contact list feature with Supabase integration and UI enhancements

--- src/app/pages/contacts/contact.service.ts
+++ src/app/pages/contacts/contact.service.ts
@@ -1,6 +1,64 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core'
+import { createClient } from '@supabase/supabase-js'
 
 @Injectable({
   providedIn: 'root',
 })
-export class ContactService {}
+export class Supabase {
+
+  supabaseUrl = 'https://xghgnrqmiunojpfilsyr.supabase.co'
+  supabaseKey = 'sb_publishable_fq87MyT8emzNGArQ2tHwHA_9DRDSjD9'
+
+  supabase = createClient(this.supabaseUrl, this.supabaseKey)
+
+  contacts = signal<{
+    id?: number
+    name: string
+    email: string
+    phone: number
+  }[]>([])
+
+  async getContacts() {
+    const { data, error } = await this.supabase
+      .from('contacts')
+      .select('*')
+      .order('name', {ascending: true})
+
+    if (error) {
+      console.error(error)
+      return
+    }
+
+    if (data) {
+      this.contacts.set(data)
+    }
+  }
+
+  subscribeToChanges(){
+    this.supabase
+    .channel('contacts-changes')
+    .on(
+      'postgres_changes',
+      {
+        event: '*',
+        schema: 'public',
+        table: 'contacts',
+
+      },
+      () => {
+        this.getContacts();
+      }
+    )
+    .subscribe();
+  }
+
+  async addContact (name: string, email: string, phone: number) {
+    const { error } = await this.supabase
+    .from('contacts')
+    .insert ([{ name, email, phone}])
+
+    if (error) {
+      console.error(error)
+    }
+  }
+}
