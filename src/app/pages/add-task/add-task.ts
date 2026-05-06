61490f66b924e5112caac0ebc4bc64d5d617bb6e
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-05-06 18:40:45 +0200

    injected Taskservice , categories, dropdownopen..

--- src/app/pages/add-task/add-task.ts
+++ src/app/pages/add-task/add-task.ts
@@ -2,18 +2,28 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Supabase } from '../contacts/contact.service';
+import { AvatarComponent } from '../../components/avatar/avatar.component';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, AvatarComponent],
   templateUrl: './add-task.html',
   styleUrl: './add-task.scss',
 })
 export class AddTask implements OnInit {
 
   contactService = inject(Supabase);
+  taskService = inject(TaskService);
+
   contacts = this.contactService.contacts;
+  categories = this.taskService.categories;
+
+  selectedContacts: any[] = [];
+  maxVisibleContacts = 3;
+  dropdownOpen = false;
+  moreContactsOpen = false;
 
   taskForm: FormGroup;
 
@@ -23,14 +33,41 @@ export class AddTask implements OnInit {
       description: [''],
       dueDate: ['', Validators.required],
       priority: ['Medium'],
-      assignedTo: [null],
+      assignedTo: [[]],
       category: ['', Validators.required],
-      subtasks: ['']
+      subtasks: [''],
     });
   }
 
-  ngOnInit() {
-    this.contactService.getContacts();
+
+  async ngOnInit(){
+
+  await Promise.all ([
+    this.contactService.getContacts(),
+    this.taskService.getCategories()
+  ]);}
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  toggleMoreContacts() {
+    this.moreContactsOpen = !this.moreContactsOpen;
+  }
+
+  toggleContact(contact: any) {
+    const exists = this.selectedContacts.find((c) => c.id === contact.id);
+
+    if (exists) {
+      this.selectedContacts = this.selectedContacts.filter((c) => c.id !== contact.id);
+    } else {
+      this.selectedContacts.push(contact);
+    }
+
+    this.taskForm.patchValue({
+      assignedTo: this.selectedContacts.map(c => c.id),
+    });
+
   }
 
   setPriority(value: string) {
@@ -47,8 +84,9 @@ export class AddTask implements OnInit {
 
   clear() {
     this.taskForm.reset({
-      priority: 'Medium'
+      priority: 'Medium',
     });
-  }
 
+    this.selectedContacts = [];
+  }
 }
