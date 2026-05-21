6fe59e11503602568a4627531d5a12484ff3e46b
Julia Keller <juliakeller.vk@gmail.com>
2026-05-21 20:05:58 +0200

    fix: make authGuard asynchronous to properly load user session

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,9 +2,13 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = async () => {
   const auth = inject(AuthService);
   const router = inject(Router);
+
+  if (!auth.currentUser()) {
+    await auth.loadSession();
+  }
 
   if (auth.currentUser()) {
     return true;
